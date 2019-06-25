55b10e4f02affcb9df1c531688e2cf9ce746c0e0
Zac Portillos <zacpg@comcast.net.comgit config --global user.name Zachary>
2019-06-24 18:16:13 -0600

    changes to routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,56 +1,37 @@
-// ===============================================================================
-// LOAD DATA - From Hot Restraurant Example
-// Linking routes to the friend "data" source.
-// ===============================================================================
-var path = require('path');
-var friends = require("../data/friends.js");
+var friends = require("../data/friends");
 
+module.exports = function(app){
+	app.get("api/friends", function(req, res){
+		res.json(friendsList);
+	})
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-module.exports = function(app) {
-  // API GET Request
-  // ---------------------------------------------------------------------------
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
+// Create New Characters - takes in JSON input
+app.post("/api/friends", function(req, res) {
+	var newFriend = req.body;
+	var newScore = 0;
+	var total = 0;
+	var match = {
+		name: "",
+		photo: "",
+		difference: 10000
+	}
 
+	// Calculating totals 
+	for (var i = 0; i < friendsList.length; i++) {
+		total = 0;
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
+		for (var j = 0; j < friendsList[i].preferences.length; j++) {
+			total += Math.abs(friendsList[i].preferences[j] - newFriend.preferences[j]);
 
-  app.post("/api/friends", function(req, res) {
-  // will handle the incoming survey results and compatibility logic
-   var userInput = req.body;
-      var userResponse = userInput.scores;
-      var match = {
-        name: "",
-        photo: "",
-        difference: 500
-      };
-
-      for (var i = 0; i < friends.length; i++) {
-        var totalDifference = 0;
-        for (var j = 0; j < userResponse.length; j++) {
-          totalDifference += Math.abs(friends[i].scores[j] - userResponse[j]);
-          
-          if (totalDifference <= match.difference){
-              match.name = friends[i].name;
-              match.photo = friends[i].photo;
-              match.difference = totalDifference;
-          }
-        }
-      }
-
-      friends.push(userInput);
-
-      res.json(match);
-
-    });
-  };
+			if (total <= match.difference) {
+				match.name = friendsList[i].name,
+				match.photo = friendsList[i].photo,
+				match.difference = total
+			}
+    	}
+    }
+    friends.push(newFriend);
+    res.json(match);
+    console.log(match);
+});
+}
