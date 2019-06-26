935a249c042a99e2409b262929021d44e701c9ff
Zac Portillos <zacpg@comcast.net.comgit config --global user.name Zachary>
2019-06-26 17:54:30 -0600

    fix api routing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,91 +1,109 @@
-
+    
 // ===============================================================================
 // DATA
-// Below data will hold all of the friends
+// Below data will hold all of the possible friends.
 // ===============================================================================
 
-var friends = [
-    {
-      name:"Bob Dylan",
-      photo:"https://cdn.pastemagazine.com/www/articles/2019/03/05/dylan-schatzberg-main.jpg",
-      scores:[
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Elon Musk",
-      photo: "https://en.wikipedia.org/wiki/Elon_Musk#/media/File:Elon_Musk_2015.jpg",
-      scores: [
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "John Lennon",
-      photo: "http://www.johnlennon.com/wp-content/uploads/2018/02/Walls_Bridges_1974_Gruen_JohnLennon-home-slider-min.jpg",
-      scores: [
-        "3",
-        "2",
-        "1",
-        "3",
-        "2",
-        "1",
-        "3",
-        "2",
-        "1",
-        "5"
-      ]
-    },
-    {
-      name: "Mick Jagger",
-      photo: "https://pbs.twimg.com/profile_images/890611271028137984/_5xAXOi-.jpg",
-      scores: [
-        "5",
-        "4",
-        "3",
-        "2",
-        "1",
-        "5",
-        "4",
-        "3",
-        "2",
-        "1"
-      ]
-    },
-    {
-      name: "Bono",
-      photo: "https://camerawork.de/wp-content/uploads/2019/02/Bono_FORBES_062217_2_FR12_F.jpg",
-      scores: [
-        "5",
-        "4",
-        "3",
-        "2",
-        "1",
-        "5",
-        "4",
-        "3",
-        "2",
-        "1"
-      ]
-    }
-  ]
 
-// Note how we export the array. This makes it accessible to other files using require.
+var friends = [
+  {
+     "name":"Ahmed",
+     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+     "scores":[
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+     ]
+  },
+  {
+     "name":"Jacob Deming",
+     "photo":"https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+     "scores":[
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+     ]
+  },
+  {
+     "name":"Jeremiah Scanlon",
+     "photo":"https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+     "scores":[
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+     ]
+  },
+  {
+     "name":"Louis T. Delia",
+     "photo":"https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+     "scores":[
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+     ]
+  },
+ {
+    "name":"Lou Ritter",
+    "photo":"https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+    "scores":[
+       "4",
+       "3",
+       "4",
+       "1",
+       "5",
+       "2",
+       "5",
+       "3",
+       "1",
+       "4"
+    ]
+ },
+ {
+    "name":"Jordan Biason",
+    "photo":"https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+    "scores":[
+       "4",
+       "4",
+       "2",
+       "3",
+       "2",
+       "2",
+       "3",
+       "2",
+       "4",
+       "5"
+    ]
+ }
+
+]
+
+// Here we export the array. This makes it accessible to other files using require. 
 module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,31 @@
 // ===============================================================================
-// DEPENDENCIES - From Hot Restraurant Example
-// Include the path package to get the correct file path
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
 // ===============================================================================
-var path = require("path");
+var path = require('path');
+
+
+
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
 
 module.exports = function(app){
-// HTML : Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "/../public/home.html"));
-});
 
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "/../public/survey.html"));
-});
-};
+	// HTML GET Requests
+	// Below code handles when users "visit" a page. 
+	// In each of the below cases the user is shown an HTML page of content
+	// ---------------------------------------------------------------------------
+
+	app.get('/survey', function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/survey.html'));
+	});
+
+	// If no matching route is found default to home
+	app.use(function(req, res){
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+}
